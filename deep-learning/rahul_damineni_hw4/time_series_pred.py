255b9314ffe81c61c379875dd0efc980092adae6
RahulDamineni <daminenirahul@gmail.com>
2020-03-05 01:16:30 -0800

    dl.hw4.(rewrote data-prep using torch.utils.data.Dataset)

--- deep-learning/rahul_damineni_hw4/time_series_pred.py
+++ deep-learning/rahul_damineni_hw4/time_series_pred.py
@@ -1,29 +1,45 @@
 import pandas as pd
 import torch
 
-df = pd.read_csv("./AirPassengers.csv")
-df = df.sort_values(by=["Month"])
 
-x = df.AirPassengers
-y = df.AirPassengers.shift(periods=-1, fill_value=0)
+class AirPassengersDataset(torch.utils.data.Dataset):
 
+    def __init__(self, path_to_csv, train=True, train_per=0.75):
 
-train_count = int(0.75 * len(df))
-x_train = torch.tensor(x[:train_count].values)
-y_train = torch.tensor(y[:train_count].values)
-x_test = torch.tensor(x[train_count:].values)
-y_test = torch.tensor(y[train_count:].values)
+        df = pd.read_csv(path_to_csv)
 
-# import ipdb
-# ipdb.set_trace()
-train = torch.utils.data.TensorDataset(x_train, y_train)
-test = torch.utils.data.TensorDataset(x_test, y_test)
+        train_count = int(0.75 * len(df))
+        if train is True:
+            df = df[:train_count]
+        else:
+            df = df[train_count:]
+
+        self.x = df.AirPassengers
+        self.y = df.AirPassengers.shift(periods=-1, fill_value=0)
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        sample = {
+            "x": self.x.iloc[idx],
+            "y": self.y.iloc[idx]
+        }
+
+        return sample
+
 
 BATCH_SIZE = 10
+train = AirPassengersDataset(path_to_csv="./AirPassengers.csv", train=True)
 train_loader = torch.utils.data.DataLoader(train, batch_size=BATCH_SIZE)
+
+test = AirPassengersDataset(path_to_csv="./AirPassengers.csv", train=False)
 test_loader = torch.utils.data.DataLoader(test, batch_size=BATCH_SIZE)
 
 for i, data in enumerate(train_loader):
 
-    x, y = data
+    x, y = data["x"], data["y"]
     print(f'Mini-batch: {i} | X: {x.size()}, Y: {y.size()}')
